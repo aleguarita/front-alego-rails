74a423e69d4248bf68d976f7d522e268d50ae997
Alessandro Guarita <alessandro@assembleia.go.gov.br>
2016-08-18 07:52:09 -0300

    Deletada pasta de config
    Estava sobrepondo à do usuário

--- lib/front_alego/version.rb
+++ lib/front_alego/version.rb
@@ -1,3 +1,13 @@
 module FrontAlego
-  VERSION = "0.4.1"
+  VERSION = "0.4.2"
 end
+# ==============================================================================
+# CHANGELOG
+# ==============================================================================
+# 0.4.2 - deletada pasta "config"
+# 0.4.1 - inclusão das imagens no precompile
+# 0.4.0 - correção de compatibilidade com o Firefox nos formulários
+#         movida a pasta de images para o lugar certo
+#         acrescentada imagem de permissao
+# 0.3.1 - correções para funcionar e ajustes de propriedade do arquivo
+# 0.3.0 - criada a gem
