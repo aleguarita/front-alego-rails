41f47c3cab176e383eb89a7a9597e8f0ec93f954
Alessandro Guarita <alessandro@assembleia.go.gov.br>
2016-08-19 10:53:08 -0300

    Inclusão de Loading e outras alterações

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w( ico.svg )
 Rails.application.config.assets.precompile += %w( permissao.svg )
+Rails.application.config.assets.precompile += %w( loading.svg )
--- lib/front_alego/version.rb
+++ lib/front_alego/version.rb
@@ -1,9 +1,12 @@
 module FrontAlego
-  VERSION = "0.4.2"
+  VERSION = "1.0.0"
 end
 # ==============================================================================
 # CHANGELOG
 # ==============================================================================
+# 1.0.0 - Acrescentada tela de loading
+#         Acréscimo de mensagem de erro mais adequada para cor() errada
+#         Mudei a versão pq era ter sido essa desde o início
 # 0.4.2 - deletada pasta "config"
 # 0.4.1 - inclusão das imagens no precompile
 # 0.4.0 - correção de compatibilidade com o Firefox nos formulários
