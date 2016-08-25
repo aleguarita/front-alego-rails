36501a50331e953d29cfdf92aa9ae8385a66fb6a
Alessandro Guarita <alessandro@assembleia.go.gov.br>
2016-08-25 08:53:30 -0300

    Pequenos ajuste

--- lib/front_alego/version.rb
+++ lib/front_alego/version.rb
@@ -1,12 +1,16 @@
 module FrontAlego
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 end
 # ==============================================================================
 # CHANGELOG
 # ==============================================================================
+# 1.0.1 - Pequenas correções:
+#         * retirado aviso de incompatibilidade com uso de unquote()
+#         * retirada a cor cinza da zebra da tabela tipo-1
+#         * Ajuste nos botões para quando ele for um link (a.bt)
 # 1.0.0 - Acrescentada tela de loading
 #         Acréscimo de mensagem de erro mais adequada para cor() errada
-#         Mudei a versão pq era ter sido essa desde o início
+#         Mudei a versão pq era pra ter sido essa desde o início
 # 0.4.2 - deletada pasta "config"
 # 0.4.1 - inclusão das imagens no precompile
 # 0.4.0 - correção de compatibilidade com o Firefox nos formulários
@@ -14,3 +18,9 @@ end
 #         acrescentada imagem de permissao
 # 0.3.1 - correções para funcionar e ajustes de propriedade do arquivo
 # 0.3.0 - criada a gem
+
+
+# REGRAS PARA VERSÃO:
+# Primeiro número é a versão maior (vai demorar a mudar)
+# O segundo é para a inclusão de funcionalidades ou de ajustes que interferem no uso (nova classe, nova variável, alguma coisa que podemos usar no SCSS ou HTML; exemplo: tela de loading).
+# O terceiro digito é para os ajustes que não interferem no uso do front-alego
