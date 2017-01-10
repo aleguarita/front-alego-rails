a5811b8c02d38ec28de213523d1f44566e56d85d
Alessandro Guarita <alessandro.guarita@gmail.com>
2017-01-10 10:16:52 -0200

    Versão 1.9.1

--- lib/front_alego/version.rb
+++ lib/front_alego/version.rb
@@ -1,9 +1,10 @@
 module FrontAlego
-  VERSION = "1.9.0"
+  VERSION = "1.9.1"
 end
 # ==============================================================================
 # CHANGELOG
 # ==============================================================================
+# 1.9.1 - Alteração de erro de cores
 # 1.9.0 - Formatação das diretivas de menu e header-tabela-mutante (barra de ferramentas)
 # 1.8.1 - Incluída diretiva de menu (formatação vai sair na versão 1.9.0)
 # 1.8.0 - Inclusão da diretiva header-tabela-mutante
