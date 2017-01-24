9aabeefb3727089d0411ea44e5c669983c2815fa
Alessandro Guarita <alessandro.guarita@gmail.com>
2017-01-24 11:06:18 -0200

    Versão 1.11.9

--- lib/front_alego/version.rb
+++ lib/front_alego/version.rb
@@ -1,9 +1,10 @@
 module FrontAlego
-  VERSION = "1.11.8"
+  VERSION = "1.11.9"
 end
 # ==============================================================================
 # CHANGELOG
 # ==============================================================================
+# 1.11.9 - Diretiva de menu atualizada
 # 1.11.8 - Arrumado o arquivo de fontes para o padrão correto do rails
 # 1.11.7 - Tirada funções das variáveis
 # 1.11.6 - Temporiamente restaurado o arquivo de fonte da GEM
